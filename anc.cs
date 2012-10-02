22ad0a4e99e981c072106928c9af59b05ab3ec51
Skish <solanki.aakash@gmail.com>
2012-10-02 10:31:36 +0530

    Add Category Dialog and Combo Box Binding Ready!

--- anc.cs
+++ anc.cs
@@ -6,6 +6,11 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data.SqlServerCe;
+using System.Data.Common;
 
 namespace WpfApplication3
 {
@@ -20,10 +25,30 @@ namespace WpfApplication3
         {
 
         }
+        public class category
+        {
+            public string Description { get; set; }
+
+            public void InsertCat(string Name)
+            {
+                SqlCeConnection Conn = new SqlCeConnection("Data Source=D:\\TSLU\\MyDatabase4.sdf");
+                Conn.Open();
+                SqlCeCommand Cmd = new SqlCeCommand();
+                Cmd.Connection = Conn;
+                Cmd.CommandText = "Insert into Category (Description)Values(@Description)";
+                Cmd.Parameters.AddWithValue("@Description", Description);
+                Cmd.ExecuteNonQuery();
+                Conn.Close();
+
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            category c = new category();
+            c.Description= textBox1.Text;
+            c.InsertCat(c.Description);
+            Close();
         }
     }
 }

d1f7fa2e54afa8b3166b2cc5be0a8e6715c9fe99
Skish <solanki.aakash@gmail.com>
2012-12-15 14:46:15 +0530

    combobox bound in organizations table and respective GUI mapped to database

--- addorg.cs
+++ addorg.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlServerCe;
+using System.Data.SqlClient;
 
 namespace WpfApplication3
 {
@@ -14,6 +16,38 @@ namespace WpfApplication3
         public addorg()
         {
             InitializeComponent();
+            
+        }
+
+        public string name { get; set; } 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+            SqlCeConnection Conn = new SqlCeConnection("Data Source=D:\\TSLU\\MyDatabase4.sdf");
+            Conn.Open();
+            SqlCeCommand Cmd = new SqlCeCommand();
+            Cmd.Connection = Conn;
+            Cmd.CommandText = "Insert into Organization (Name)Values(@Name)";
+            Cmd.Parameters.AddWithValue("@Name", name);
+            Cmd.ExecuteNonQuery();
+            Conn.Close();
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            name = textBox1.Text;
+            SqlCeConnection Conn = new SqlCeConnection("Data Source=D:\\TSLU\\MyDatabase4.sdf");
+            Conn.Open();
+            SqlCeCommand Cmd = new SqlCeCommand();
+            Cmd.Connection = Conn;
+            Cmd.CommandText = "Insert into Organizations (Name)Values(@Name)";
+            Cmd.Parameters.AddWithValue("@Name", textBox1.Text);
+            Cmd.ExecuteNonQuery();
+            Conn.Close();
+            Close();
         }
     }
 }
